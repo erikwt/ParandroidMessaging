946513b422cc03b35b3cbe97d9ab52eec64b4777
Erik Wallentinsen <mail@erikw.eu>
2010-04-07 03:26:26 +0200

    Fixed notifications
    Fixed notifications for receiving public keys and encrypted messages

---
+++ src/org/parandroid/sms/ui/EncryptedMessageNotificationActivity.java
@@ -0,0 +1,25 @@
+package org.parandroid.sms.ui;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class EncryptedMessageNotificationActivity extends Activity {
+	
+	private static final String TAG = "Parandroid EncryptedMessageNotificationActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent i = getIntent();
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.cancel(i.getIntExtra("notificationId", MessageUtils.DEFAULT_NOTIFICATION_ID));
+
+		Intent targetIntent = new Intent(getApplicationContext(), ComposeMessageActivity.class);
+		targetIntent.putExtra("threadId", -1);
+		startActivity(targetIntent);
+	}
+}
