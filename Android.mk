a3307b029c74443578f1f6c0a313cab0f6a0a810
Jeffrey <jeffrey@desktop-jef.localdomain>
2010-04-18 17:26:57 +0200

    initial commit: forked mms app, and renamed to org.parandroid.sms

---
+++ Android.mk
@@ -0,0 +1,15 @@
+# Copyright 2007-2008 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := ParandroidMessaging
+
+LOCAL_REQUIRED_MODULES := SoundRecorder
+
+include $(BUILD_PACKAGE)
+
+# This finds and builds the test apk as well, so a single make does both.
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ src/org/parandroid/sms/MmsApp.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2008 Esmertec AG.
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.parandroid.sms;
+
+import org.parandroid.sms.drm.DrmUtils;
+import org.parandroid.sms.layout.LayoutManager;
+import org.parandroid.sms.util.ContactInfoCache;
+import org.parandroid.sms.util.DownloadManager;
+import org.parandroid.sms.util.DraftCache;
+import org.parandroid.sms.util.SmileyParser;
+import org.parandroid.sms.util.RateController;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+
+public class MmsApp extends Application {
+    public static final String LOG_TAG = "Mms";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Load the default preference values
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        ContactInfoCache.init(this);
+        DraftCache.init(this);
+        DownloadManager.init(this);
+        RateController.init(this);
+        DrmUtils.cleanupStorage(this);
+        LayoutManager.init(this);
+        SmileyParser.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        DrmUtils.cleanupStorage(this);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        LayoutManager.getInstance().onConfigurationChanged(newConfig);
+    }
+}
